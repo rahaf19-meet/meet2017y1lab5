da65e670b8c18d103f49c96c538db517485e0fc0
rahaf19-meet <rahaf19@meet.mit.edu>
2017-07-30 10:55:21 +0200

    Stopping here

---
+++ fun2.py
@@ -0,0 +1,16 @@
+#print("*")
+#print("*")
+#print("*")
+#print("*")
+#print("*")
+#print("*" *5)
+
+#def draw_1d(n,char):
+    #print(char*n)
+#def draw_reactangle(n1,n2):
+    #for i in range(n1):
+        #print('*'*n2)
+def draw_2d(n,m,char1):
+    for i in range(n):
+        print(char1*m)
+    
